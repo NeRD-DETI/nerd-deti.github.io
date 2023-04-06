ce77865b63795a50903176f934340d06d47c4163
Hugo Leal <hugolardosa@gmail.com>
2023-04-06 12:16:11 +0100

    changing for nerd.aettua.pt

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  homepage: "https://nerd-deti.github.io",
+  homepage: "https://nerd.aettua.pt",
   define: {
     'process.env': process.env
   },
